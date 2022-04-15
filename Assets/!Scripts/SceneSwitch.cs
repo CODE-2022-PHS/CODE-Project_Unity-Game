db59b61a4f90f8e2d18ce1538559f2b3a9e1e1fb
irvin1226 <IrvinOrtiz805@gmail.com>
2022-04-14 19:02:18 -0700

    Uploaded Unity Project to GitHub

---
+++ Assets/!Scripts/SceneSwitch.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [SerializeField] KeyCode interactKey;
+    [SerializeField] bool hasCollided = false;
+    [SerializeField] string textLabel = "Hold F to exit level";
+    
+    void OnGUI()
+        {
+            if(hasCollided == true)
+            {
+                GUI.Box(new Rect(50, Screen.height - 50, Screen.width - 150, 120), (textLabel));
+            }
+        }
+
+    void OnTriggerEnter(Collider other)
+    {
+        hasCollided = true;
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        hasCollided = false;
+    }
+
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    */
+    // Update is called once per frame
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(interactKey) && hasCollided)
+        {
+            SceneManager.LoadScene("OpenWorld1");
+        }
+    }
+    
+}
