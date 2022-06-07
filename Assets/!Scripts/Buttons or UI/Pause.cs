3bc0b4a2c1614bca07c0a0a79a59d2f09fe31aed
irvin1226 <IrvinOrtiz805@gmail.com>
2022-06-07 12:51:54 -0700

    Sensitivity Slider Added
    * Add quiver pickups
     - more arrows
    * collect items before leaving
    * fix slider not carrying over
    * health pack (1 or 2)

--- Assets/!Scripts/Buttons or UI/Pause.cs
+++ Assets/!Scripts/Buttons or UI/Pause.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject slider;
+    public Text sensitivityText;
+
+
     public bool isPaused = false;
+
 
     // Update is called once per frame
     void Update()
     {
+        sensitivityText.text = GlobalData.sensitivity.ToString("f2");
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             Time.timeScale = 0;
             isPaused = true;
             pauseMenu.SetActive(true);
+            slider.SetActive(true);
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
@@ -24,6 +32,7 @@ public class Pause : MonoBehaviour
     {
         Time.timeScale = 1;
         pauseMenu.SetActive(false);
+        slider.SetActive(false);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
